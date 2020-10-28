a07ebf298c3946c47483ff2a0e02b135567522d3
Andrew Barkovets <andrewbarkovets@gmail.com>
2020-10-28 11:02:32 +0300

    fix(main.js):new fix

--- script/main.js
+++ script/main.js
@@ -5,12 +5,12 @@ let todoControl = document.querySelector('.todo-control'),
     headerBtn = document.querySelector('.header-button'),
     todoList = document.querySelector('.todo-list'),
     textTodo = document.querySelector('.text-todo'),
+    removeBtn = document.querySelector('.todo-remove'),
     todoCompleted = document.querySelector('.todo-completed');
 
 // Массив с получеными данными о планах
-const todoData = [];
-
-let jsonToDo = JSON.stringify(todoData);
+let todoData = localStorage.getItem('item') ?
+ JSON.parse(localStorage.getItem('item')) : [];
 
 // Добавление дел на страницу
 const render = function() {
@@ -21,20 +21,20 @@ const render = function() {
         let li = document.createElement('li');
         li.classList.add('todo-item');
 
-        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+        li.innerHTML += '<span class="text-todo">' + item.value + '</span>' +
             '<div class="todo-buttons">' +
                 '<button class="todo-remove"></button>' +
                 '<button class="todo-complete"></button>' +
             '</div>';
-        
+
+        // Метка о выполнении
+        const btnComplete = li.querySelector('.todo-complete');
+
         if(item.completed) {
             todoCompleted.append(li);
         } else {
             todoList.append(li);
         }
-
-        // Метка о выполнении
-        const btnComplete = li.querySelector('.todo-complete');
 
         // Добавление в выполненые(не выполненые)
         btnComplete.addEventListener('click', function() {
@@ -43,13 +43,8 @@ const render = function() {
         });
 
     });
+ 
 };
-
-// LocalStorage
-headerBtn.addEventListener('click', function() {
-    localStorage.textTodo = headerInput.value;
-});
-
 
 // К форме todoControl навешиваем событие submit
 todoControl.addEventListener('submit', function(e) {
@@ -61,8 +56,29 @@ todoControl.addEventListener('submit', function(e) {
     };
 
     todoData.push(newTodo);
-
+    
+    localStorage.setItem('item', JSON.stringify(todoData));
+    headerInput.value = '';
+     
     render();
+ 
 });
 
+// Удаление дел
+// removeBtn.addEventListener('click', function() {
+//     todoData.forEach(function(i) {
+
+//     };
+// });
+
 render();
+
+
+
+
+
+// todoList.addEventListener('click', function(e) {
+//         const target = e.target;
+//         const removeBtn = target.closest('.todo-remove');
+//         console.log(removeBtn);
+//     });
